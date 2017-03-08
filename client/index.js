b0a073bc63fb77806fb303e697b0ff50bfe342da
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-03-08 17:57:49 +0000

    Updating everything to work

--- client/index.js
+++ client/index.js
@@ -1,37 +1,40 @@
-/*
-  TODO: delete this comment
-
-  This file is where you bootstrap your JS code
-  For example import stuff here:
-
-  import {select} from 'd3-selection';
-  import myComponent from './components/my-component';
-
-  Split logical parts of you project into components e.g.
-
-  /client
-    - /components
-        - /component-name
-            - styles.scss
-            - index.js
-            - template.html
-
-*/
-
 import $ from 'jquery';
-import tablesort from 'tablesort';
+import Tablesort from 'tablesort';
 
-$('.search').keyup(function () {
-  var _this = this;
-        // Show only matching TR, hide rest of them
-  $.each($('.sortable tbody tr'), function () {
-    if ($(this).text().toLowerCase().indexOf($(_this).val().toLowerCase()) === -1)
+// Taken and rewritten from https://github.com/tristen/tablesort/blob/gh-pages/src/sorts/tablesort.number.js
+(function sortNumber() {
+  const cleanNumber = i => i.replace(/[^\-?0-9.]/g, '');
+
+  const compareNumber = (a, b) => {
+    const floatA = parseFloat(a);
+    const floatB = parseFloat(b);
+
+    return (isNaN(floatA) ? 0 : floatA) - (isNaN(floatB) ? 0 : floatB);
+  };
+
+  const suffixedCurrency = /^-?\d+\s*([,.]\d{0,2})?[£\x24Û¢´€]/; // Suffixed currency
+  const numberRegex = /^-?(\d)*-?([,.]){0,1}-?(\d)+([E,e][-+][\d]+)?%?$/; // General number
+  const prefixed = /^-?[£\x24Û¢´€]?\d+\s*([,.]\d{0,2})/; // Prefixed currency
+
+  Tablesort.extend(
+    'number',
+    item => prefixed.test(item) || suffixedCurrency.test(item) || numberRegex.match(item),
+    (a, b) => compareNumber(cleanNumber(b), cleanNumber(a)),
+  );
+}());
+
+$('.search').keyup(() => {
+  const el = this;
+  // Show only matching TR, hide rest of them
+  $.each($('.sortable tbody tr'), function handleSearch() {
+    if ($(this).text().toLowerCase().indexOf($(el).val().toLowerCase()) === -1) {
       $(this).hide();
-    else
+    } else {
       $(this).show();
+    }
   });
 });
 
-tablesort(document.querySelector('.sortable'), {
+Tablesort(document.querySelector('.sortable'), {
   // descending: true,
 });
