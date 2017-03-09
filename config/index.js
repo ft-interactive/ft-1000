b2d1b6489577afb1067ed9588ec0e8eec72cec6f
Ændrew Rininsland <andrew.rininsland@ft.com>
2017-03-09 11:22:40 +0000

    Fixes search, sorting

--- config/index.js
+++ config/index.js
@@ -8,26 +8,21 @@ export default async () => {
   const d = await article();
   const flags = await getFlags();
   const onwardJourney = await getOnwardJourney();
-  /*
-  An experimental demo that gets content from the API
-  and overwrites some model values. This requires the Link File
-  to have been published. Also next-es-interface.ft.com probably
-  isn't a reliable source. Also this has no way to prevent development
-  values being seen in productions... use with care.
-
-  */
 
   try {
     const table = (await axios('https://bertha.ig.ft.com/view/publish/gss/1sLtLlvhaCDBgGSNskJPdKdkWJFwFNq0Er6GMGaEbt9c/data')).data;
-  return {
-    ...d,
-    flags,
-    onwardJourney,
-    table
-  };
+    return {
+      ...d,
+      flags,
+      onwardJourney,
+      table,
+    };
   } catch (e) {
-    console.log('Error getting content from Bertha');
+    console.error('Error getting content from Bertha'); // eslint-disable-line
+    return {
+      ...d,
+      flags,
+      onwardJourney,
+    };
   }
-
-
 };
