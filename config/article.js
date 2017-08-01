57736d8a7ab955a2aa431285f385725d40c75eab
Martin Stabe <martinstabe@gmail.com>
2017-08-01 13:17:05 +0100

    fix broken ads

--- config/article.js
+++ config/article.js
@@ -77,11 +77,13 @@ facebookImage: 'https://ig.ft.com/ft-1000/images/FT1000_logo.png',
 facebookHeadline: 'The FT 1000: Europe’s fastest-growing companies',
 
 //ADVERTISING
-ads: {
-  // ad unit hierarchy makes ads more granular. Start with ft.com and /companies /markets /world
-  gptAdunit: 'ft.com/companies/european',
-  // granular targeting is optional
-  dftTargeting: "sr=FT 1000: Europe's Fastest Growing Companies",
+ ads: {
+    // Ad unit hierarchy makes ads more granular.
+    gptSite: 'ft.com',
+    // Start with ft.com and /companies /markets /world as appropriate to your story
+    gptZone: 'companies/european',
+    // granular targeting is optional and will be specified by the ads team
+    dfpTargeting: "sr=FT 1000: Europe's Fastest Growing Companies",
 },
 
   tracking: {
