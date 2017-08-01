56facc3bbff262bce9adf1a3536d2f59023ab87b
Martin Stabe <martinstabe@gmail.com>
2017-08-01 13:35:45 +0100

    Fixed publish date

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date(),
+  publishedDate: new Date('2017-05-17T18:11:32.765Z'),
 
   headline: 'The FT 1000: The complete list of Europe’s fastest-growing companies',
 
