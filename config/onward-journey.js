ba11907db439b3077939037209cc67a684184b11
Martin Stabe <998519+martinstabe@users.noreply.github.com>
2017-08-20 10:18:04 +0100

    Update OJ to v2

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
    // { rows: 1, list: 'thing/NWNmNzM1NmYtOTgzYy00MGM2LTllZDgtZGI1YjM0YTBlYzc2-U3BlY2lhbFJlcG9ydHM=' },
-    { rows: 2, list: 'thing/MTM1-U2VjdGlvbnM=' },
+    { rows: 2, list: 'thing/f294c389-8e8c-3e92-9ccf-ea1ce61b1bcd' },
   ],
 });
