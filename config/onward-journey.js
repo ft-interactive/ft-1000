d4f91358d8408a3af9800bbf8cdf9a3ad5f0ce18
David Blood <davidbjourno@users.noreply.github.com>
2018-02-09 13:01:34 +0000

    Update onward journey; misc. styling and layout

--- config/onward-journey.js
+++ config/onward-journey.js
@@ -1,6 +1,6 @@
 export default () => ({
   relatedContent: [
    // { rows: 1, list: 'thing/NWNmNzM1NmYtOTgzYy00MGM2LTllZDgtZGI1YjM0YTBlYzc2-U3BlY2lhbFJlcG9ydHM=' },
-    { rows: 2, list: 'thing/f294c389-8e8c-3e92-9ccf-ea1ce61b1bcd' },
+    { rows: 2, list: 'thing/94977e6e-f170-34c8-b6db-dbbb4a9c6137' },
   ],
 });
