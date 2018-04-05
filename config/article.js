595640b52645da9779b080e43d7c6ac9e6a8f9c9
David Blood <davidbjourno@users.noreply.github.com>
2018-04-05 12:34:44 +0100

    Update URL

--- config/article.js
+++ config/article.js
@@ -5,7 +5,7 @@ export default () => ({ // eslint-disable-line
 
   // canonical URL of the published page
   // https://ig.ft.com/special-reports/ft-1000/asia-pacific/2018/ get filled in by the ./configure script
-  url: 'https://ig.ft.com/ft-1000/',
+  url: 'https://ig.ft.com/ft-1000/2018/',
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
