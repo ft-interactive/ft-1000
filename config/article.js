8ec093aa3861a007cd1973635cfbd292797e23ae
David Blood <9371095+davidbjourno@users.noreply.github.com>
2018-09-21 12:41:40 +0100

    Update publication time

--- config/article.js
+++ config/article.js
@@ -9,7 +9,7 @@ export default () => ({ // eslint-disable-line
 
   // To set an exact publish date do this:
   //       new Date('2016-05-17T17:11:22Z')
-  publishedDate: new Date('2018-09-21T04:00:00Z'),
+  publishedDate: new Date('2018-09-24T04:00:00Z'),
 
   headline: 'FT Future 100 UK: the full list',
 
